d4e9c5f9ebc5b62276e9866d5be3e0a45a9a9aa3
Eadsger <150641010+Eadsger@users.noreply.github.com>
2024-04-08 22:23:15 -0300

    feat: Update EventsController to allow creation of new events with user's ID prepopulated
    - Modified EventsController to use current_user to build new events with the user's ID prepopulated.
    - Created UsersController to display events created by a specific user on their show page.
    - Updated Event and User models to establish the association between creator (user) and events.
    - Added view for displaying events created by a user on their show page.
    - Updated routes to include actions for events and users.
    - Created migration to add creator_id foreign key to events table.
    - Updated schema.rb to reflect changes from the migration.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,22 @@
 class EventsController < ApplicationController
-  def index
-    @events = Event.all
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
   end
 end
